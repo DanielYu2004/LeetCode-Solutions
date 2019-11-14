f3cbdd2f828f6af50b27e54540e7594fe2c0911e
DanielYu2004 <55118464+DanielYu2004@users.noreply.github.com>
2019-11-14 16:34:08 -0500

    Delete Two Sum.py

--- Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        check = {}
-        for i in range(len(nums)):
-            test = target - nums[i]
-            if test in check:
-                return [check[test], i]
-            else:
-                check[nums[i]] = i
